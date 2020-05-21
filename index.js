a897aac5dce55318f8f5899c6bccefac19c85a12
FahimMuntashir <fahim.tech.me@gmail.com>
2020-05-22 01:11:11 +0600

    final

--- index.js
+++ index.js
@@ -4,4 +4,21 @@ var randomImage="images/dice" +randomNum1 +".png";
 
 var image1 = document.querySelectorAll("img")[0].setAttribute("src", randomImage);
 
-var image2 = document.querySelectorAll("img")[1].setAttribute("src" , randomImage);
+
+var randomNum2 = Math.floor(Math.random()*6)+1;
+
+var randomImage2 = "images/dice" +randomNum2 +".png";
+var image2 = document.querySelectorAll("img")[1].setAttribute("src", randomImage2);
+
+
+if(randomNum1>randomNum2){
+
+  document.querySelector("h1").innerHTML = "Player 1 win";
+}
+else if (randomNum1<randomNum2) {
+  document.querySelector("h1").innerHTML = "Player 2 wins";
+
+}
+else {
+  document.querySelector("h1").innerHTML = "draw !";
+}
